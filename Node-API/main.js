c24a277035718831ff79d1be4ffe9633c62e12eb
jack young <jackjordanyoung@gmail.com>
2023-05-13 02:53:32 +0100

    basic fileserve

--- Node-API/main.js
+++ Node-API/main.js
@@ -1,7 +1,14 @@
 const express = require('express')
 const app = express()
+const port = 3000
+
+app.use(express.static(__dirname + '/godot-export'))
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', (req, res) => {
-    res.send('hello world')
+    res.sendFile(__dirname + '/godot-export/Plot-cards-godot-viewer.html')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
 })
