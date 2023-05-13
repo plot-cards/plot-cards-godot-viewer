259bc73827eab980dd9e7736791a8b438c731d35
jack young <jackjordanyoung@gmail.com>
2023-05-13 02:44:00 +0100

    updated export location

--- Node-API/main.js
+++ Node-API/main.js
@@ -1,1 +1,7 @@
-import { Express } from "express";
+const express = require('express')
+const app = express()
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', (req, res) => {
+    res.send('hello world')
+})
